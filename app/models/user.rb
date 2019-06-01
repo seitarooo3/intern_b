ec719080d29447801528452a038f6a10e7c190e0
EC2 Default User <ec2-user@ip-172-31-37-4.us-east-2.compute.internal>
2019-06-01 17:56:44 +0900

    kubo review

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
-      user = find_by(id: row["id"]) || new
+      user = find_by(name: row["name"]) || new
       
       # CSVからデータを取得し、設定する
       user.attributes = row.to_hash.slice(*updatable_attributes)
@@ -28,7 +28,7 @@ class User < ApplicationRecord
 
   # csv更新を許可するカラムを定義
   def self.updatable_attributes
-    ["id", "email","name", "affiliation", "employee_number", "uid", "basic_work_time", "designed_work_start_time", "designed_work_end_time", "superior", "admin", "password"]
+    ["email","name", "affiliation", "employee_number", "uid", "basic_work_time", "designed_work_start_time", "designed_work_end_time", "superior", "admin", "password"]
   end
   
 end
